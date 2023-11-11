7b5b3146507827bc15a4219e50e6190679a820ea
alves-patrick <patrickalvesarts@gmail.com>
2023-11-11 11:20:14 -0300

    NavigationView, SignUp, SignIn

---
+++ iChat/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  iChat
+//
+//  Created by Patrick Alves on 11/11/23.
+//
+
+import Foundation
---
+++ iChat/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  iChat
+//
+//  Created by Patrick Alves on 11/11/23.
+//
+
+import Foundation
