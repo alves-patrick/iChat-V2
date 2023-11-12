d86e103b0a21977e73698bc5b60b770663feee96
alves-patrick <patrickalvesarts@gmail.com>
2023-11-12 14:57:10 -0300

    Chat, messages

---
+++ iChat/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  iChat
+//
+//  Created by Patrick Alves on 12/11/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
