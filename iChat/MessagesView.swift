5a6bd71daf5d65588ee34230ec2bd5204a4e7512
alves-patrick <patrickalvesarts@gmail.com>
2023-11-12 10:07:26 -0300

    Login, Logout

---
+++ iChat/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  iChat
+//
+//  Created by Patrick Alves on 12/11/23.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessagesView()
+}
---
+++ iChat/MessagesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MessagesViewModel.swift
+//  iChat
+//
+//  Created by Patrick Alves on 12/11/23.
+//
+
+import Foundation
