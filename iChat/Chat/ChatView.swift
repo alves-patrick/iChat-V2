41ebeb0ec0f40694529b8cabcf2fd871831829a2
alves-patrick <patrickalvesarts@gmail.com>
2023-11-21 20:00:37 -0300

    Refactoring and Clean Code

--- iChat/Chat/ChatView.swift
+++ iChat/Chat/ChatView.swift
@@ -68,7 +68,7 @@ struct ChatView: View {
                             RoundedRectangle(cornerRadius: 24.0)
                                 .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
                         )
-                        .frame(height: (textSize.height + 50) > 100 ? 100 : textSize.height + 50)
+                        .frame(height: (textSize.height + 38) > 100 ? 100 : textSize.height + 38)
                     
                     Text(viewModel.text)
                         .opacity(0)
@@ -86,7 +86,8 @@ struct ChatView: View {
                         viewModel.sendMessage(contact: contact)
                     } label: {
                         Text("Enviar")
-                            .padding()
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 8)
                             .background(Color("GreenColor"))
                             .foregroundColor(Color.white)
                             .cornerRadius(24.0)
@@ -132,12 +133,15 @@ struct MessageRow: View {
                 .lineLimit(nil)
                 .fixedSize(horizontal: false, vertical: true)
                 .padding(.horizontal,10)
-                .background(Color(white: 0.95))
-                .frame(maxWidth: 260, alignment: message.isMe ? .leading : .trailing)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(!message.isMe ? Color(white: 0.95) : Color("GreenLightColor")))
+                .frame(maxWidth: 260, alignment: !message.isMe ? .leading : .trailing)
             //.padding(.leading, message.isMe ? 0 : 50)
             //.padding(.trailing, message.isMe ? 50 : 0)
         }
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: message.isMe ? .leading : .trailing)
+        .padding(.horizontal, 2)
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: !message.isMe ? .leading : .trailing)
     }
 }
 #Preview {
